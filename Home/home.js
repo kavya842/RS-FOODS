4ba3fbff553a5a822534167df7db9645e9575eda
kavya842 <kavyavankayalapati9999@gmail.com>
2026-06-08 21:10:08 +0530

    update code

--- Home/home.js
+++ Home/home.js
@@ -1,5 +1,13 @@
-fetch("https://dummyjson.com/recipes")
-  .then(res => res.json())
-  .then(data => {
-    console.log(data.recipes);
-  });
+// Update nav badge counts
+function updateCounts() {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const order = JSON.parse(localStorage.getItem("order")) || [];
+
+  const cartTotal = cart.reduce((sum, i) => sum + (i.qty || 1), 0);
+  const orderTotal = order.reduce((sum, i) => sum + (i.quantity || 1), 0);
+
+  document.getElementById("cartCount").textContent = cartTotal;
+  document.getElementById("orderCount").textContent = orderTotal;
+}
+
+updateCounts();
